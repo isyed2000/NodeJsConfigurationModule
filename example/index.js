82de08182306c80b1db30a83e7fbedc6ece12dc0
Iftikhar Ali <iali@billtrust.com>
2019-12-20 13:08:33 -0500

    'Capitalization'

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 var configuration = require("../configurationBuilder");
 
-console.log("Configuratin loader demo.");
+console.log("Configuratin Loader Demo.");
 
 configuration.addJsonFile("configs/appSettings.json", false); //requires this file.  If not found, an error will be displayed.
 configuration.addJsonFile("./connectionStrings.json", true); //Does not require this file.
